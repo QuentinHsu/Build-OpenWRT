ec15ec5c5d53768114e258a102c3231dc2458123
Quentin <21162238+QuentinHsu@users.noreply.github.com>
2021-02-02 17:07:58 +0800

    add(script): rpi4-init about auto WAN

---
+++ script/rpi4-init.sh
@@ -0,0 +1,12 @@
+
+
+#支持 iPhone、Android USB 共享网络，无需手动配置 WAN
+uci set network.usb_network=interface
+  # 树莓派4b 将 USB 网卡识别并设置成 erh1
+uci set network.usb_network.ifname='eth1 usb0'
+uci set network.usb_network.proto=dhcp
+uci set network.usb_network.type=bridge
+uci set firewall.@zone[1].network='wan wan6 usb_network'
+
+
+exit 0
