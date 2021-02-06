415f07b7217452f4e82ae0103f36694aacb0e06d
QuentinHsu <xuquentinyang@gmail.com>
2021-02-06 21:45:40 +0800

    init(DIY-Part2): RPi4B init

--- script/DIY-Part2-RPi4B-Init.sh
+++ script/DIY-Part2-RPi4B-Init.sh
@@ -1,16 +1,9 @@
-#支持 iPhone、Android USB 共享网络，无需手动配置 WAN
-# uci set network.usb_network=interface
-  # 树莓派4b 将 USB 网卡识别并设置成 erh1
-# uci set network.usb_network.ifname='eth1 usb0'
-# uci set network.usb_network.proto=dhcp
-# uci set network.usb_network.type=bridge
-# uci set firewall.@zone[1].network='wan wan6 usb_network'
-
 # 修改默认的 LAN IPv4 地址
 sed -i 's/192.168.1.1/192.168.100.1/' package/base-files/files/bin/config_generate
 
-
+#支持 iPhone、Android USB 共享网络，无需手动配置 WAN
 sed -i '/exit 0/i uci set network.usb_network=interface' package/lean/default-settings/files/zzz-default-settings
+  # 树莓派4b 将 USB 网卡识别并设置成 erh1
 sed -i "/exit 0/i uci set network.usb_network.ifname='eth1 usb0'" package/lean/default-settings/files/zzz-default-settings
 sed -i "/exit 0/i uci set network.usb_network.proto=dhcp" package/lean/default-settings/files/zzz-default-settings
 sed -i "/exit 0/i uci set network.usb_network.type=bridge" package/lean/default-settings/files/zzz-default-settings
