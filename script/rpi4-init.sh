fc5adc3979788f32d8cfdb78e852837ce32b42f9
QuentinHsu <xuquentinyang@gmail.com>
2021-02-06 10:31:04 +0800

    test(rpi4-init): about auto network

--- script/rpi4-init.sh
+++ script/rpi4-init.sh
@@ -8,3 +8,10 @@
 
 # 修改默认的 LAN IPv4 地址
 sed -i 's/192.168.1.1/192.168.100.1/' package/base-files/files/bin/config_generate
+
+
+sed -i '/exit 0/i uci set network.usb_network=interface' package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i uci set network.usb_network.ifname='eth1 usb0'" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i uci set network.usb_network.proto=dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i uci set network.usb_network.type=bridge" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i uci set firewall.@zone[1].network='wan wan6 usb_network'" package/lean/default-settings/files/zzz-default-settings
