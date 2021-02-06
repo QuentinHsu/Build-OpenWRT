2d4fbc8bfc946361ef606bcb41139d4281564039
QuentinHsu <xuquentinyang@gmail.com>
2021-02-06 09:17:46 +0800

    change(rpi4-init.sh): comment about network

--- script/rpi4-init.sh
+++ script/rpi4-init.sh
@@ -1,10 +1,10 @@
 #支持 iPhone、Android USB 共享网络，无需手动配置 WAN
-uci set network.usb_network=interface
+# uci set network.usb_network=interface
   # 树莓派4b 将 USB 网卡识别并设置成 erh1
-uci set network.usb_network.ifname='eth1 usb0'
-uci set network.usb_network.proto=dhcp
-uci set network.usb_network.type=bridge
-uci set firewall.@zone[1].network='wan wan6 usb_network'
+# uci set network.usb_network.ifname='eth1 usb0'
+# uci set network.usb_network.proto=dhcp
+# uci set network.usb_network.type=bridge
+# uci set firewall.@zone[1].network='wan wan6 usb_network'
 
 # 修改默认的 LAN IPv4 地址
 sed -i 's/192.168.1.1/192.168.100.1' package/base-files/files/bin/config_generate
