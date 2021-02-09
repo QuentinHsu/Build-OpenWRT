5e26d23dcb9ddf5afaa158713553b5e4466d7114
QuentinHsu <xuquentinyang@gmail.com>
2021-02-09 13:45:40 +0800

    feat(script): wifi setting

--- script/DIY-Part2-RPi4B-Init.sh
+++ script/DIY-Part2-RPi4B-Init.sh
@@ -8,3 +8,11 @@ sed -i "/exit 0/i uci set network.usb_network.ifname='eth1 usb0'" package/lean/d
 sed -i "/exit 0/i uci set network.usb_network.proto=dhcp" package/lean/default-settings/files/zzz-default-settings
 sed -i "/exit 0/i uci set network.usb_network.type=bridge" package/lean/default-settings/files/zzz-default-settings
 sed -i "/exit 0/i uci set firewall.@zone[1].network='wan wan6 usb_network'" package/lean/default-settings/files/zzz-default-settings
+
+# 板载 Wi-Fi 设置
+sed -i "/exit 0/i uci set wireless.radio0.country='US'" package/lean/default-settings/files/zzz-default-settings
+# Legacy = 11g(2.4G)|11a(5G)、AC = VHT20(20,40,80)、N = HT20(20,40,80)
+sed -i "/exit 0/i uci set wireless.radio0.htmode='HT40'" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i uci set wireless.default_radio0.ssid='OpenWRTRouter'" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i uci set wireless.default_radio0.encryption='psk2'" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i uci set wireless.default_radio0.key='openwrt'" package/lean/default-settings/files/zzz-default-settings
