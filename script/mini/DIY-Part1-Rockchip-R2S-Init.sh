3a260557132c5a5e7a3ba68eb294f3b7980f2a96
QuentinHsu <xuquentinyang@gmail.com>
2021-02-28 22:03:14 +0800

    add(R2S): R2S init script

---
+++ script/mini/DIY-Part1-Rockchip-R2S-Init.sh
@@ -0,0 +1,8 @@
+# 修改默认的 LAN IPv4 地址
+sed -i 's/192.168.1.1/192.168.100.1/' package/base-files/files/bin/config_generate
+
+# Add build date to index page
+export orig_version="$(cat "/etc/openwrt_release" | grep DISTRIB_DESCRIPTION | awk -F "'" '{print $2}')"
+sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" /etc/openwrt_release
+
+exit 0
