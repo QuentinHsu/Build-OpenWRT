4cbdad3ce38a6cd1c373ded3b723cf978b66ee03
QuentinHsu <xuquentinyang@gmail.com>
2021-03-01 22:29:12 +0800

    fix(R2S): error in how compile time was added

--- script/mini/DIY-Part1-Rockchip-R2S-Init.sh
+++ script/mini/DIY-Part1-Rockchip-R2S-Init.sh
@@ -2,7 +2,7 @@
 sed -i 's/192.168.1.1/192.168.100.1/' package/base-files/files/bin/config_generate
 
 # Add build date to index page
-export orig_version="$(cat "/etc/openwrt_release" | grep DISTRIB_DESCRIPTION | awk -F "'" '{print $2}')"
+export orig_version="$(cat "package/base-files/files/etc/openwrt_release" | grep DISTRIB_DESCRIPTION | awk -F "'" '{print $2}')"
 sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" package/base-files/files/etc/openwrt_release
 
 
