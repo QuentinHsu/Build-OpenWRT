ad5d5dd87bec6f51ce1ce9f4c7706f98dae6f6a5
QuentinHsu <xuquentinyang@gmail.com>
2021-03-02 10:29:08 +0800

    change(R2S): adjusting the path structure

--- script/Rockchip-R2S/DIY-Part2-Rockchip-R2S.sh
+++ script/Rockchip-R2S/DIY-Part2-Rockchip-R2S.sh
@@ -5,5 +5,4 @@ sed -i 's/192.168.1.1/192.168.100.1/' package/base-files/files/bin/config_genera
 export orig_version="$(cat "package/base-files/files/etc/openwrt_release" | grep DISTRIB_DESCRIPTION | awk -F "'" '{print $2}')"
 sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" package/base-files/files/etc/openwrt_release
 
-
 exit 0
